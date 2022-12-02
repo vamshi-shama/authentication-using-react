a89e012a2a944b43aa082adfa34b0e0574d8e78d
vamshi <vamshis2017@gmail.com>
2022-12-02 23:37:45 +0530

    added new code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-
 import './App.css';
-
-const App = () => {
-  return(
-    <div className='App'>
-      
+import { Navbar } from './Component/Navbar'
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
+import Profile from './components/Profile';
+import SignUp from './Component/Signup';
+function App() {
+  return (
+    <div className="bg-black h-screen overflow-hidden w-screen  text-slate-200">
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<SignUp />} />
+          <Route path='/profile' element={<Profile />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/Component/Navbar.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+
+const Navbar = () => {
+  
+  return (
+    <div className='bg-black text-slate-200 w-full h-14 '>
+      <div className=' h-14 fixed w-full z-50 bg-black border border-transparent border-b-white'>
+        <div className=' w-[90%] m-auto flex justify-between'>
+          <a href='/'> Header</a>
+
+          <div className='flex gap-5'>
+            <a href='/'>Signup</a>
+            <a href='/Profile'>Profile</a>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
