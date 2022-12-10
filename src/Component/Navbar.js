3070c285ad36e71faa0af836939fd59a98c640a4
vamshi <vamshis2017@gmail.com>
2022-12-10 23:19:41 +0530

    trying diff method

--- src/Component/Navbar.js
+++ src/Component/Navbar.js
@@ -1,21 +1,15 @@
-import React from 'react'
-
+import React from "react";
 
 const Navbar = () => {
-  
-  return (
-    <div className='bg-black text-slate-200 w-full h-14 '>
-      <div className=' h-14 fixed w-full z-50 bg-black border border-transparent border-b-white'>
-        <div className=' w-[90%] m-auto flex justify-between'>
-          <a href='/'> Header</a>
-          <div className='flex gap-5'>
-            <a href='/'>Signup</a>
-            <a href='/Profile'>Profile</a>
-          </div>
-        </div>
-      </div>
+  return(
+    <div className="Navbar">
+          {/* <a href="/"> Header </a>
+          <a href="/"> SignUp </a>
+          <a href="/Profile"> Profile </a> */}
+
+          <link >Header</link>
     </div>
-  )
+  );
 }
 
 export default Navbar;
