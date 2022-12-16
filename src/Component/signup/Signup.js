d5bc71bd1de82faa71de1f03dedd7be2e5ef89c1
vamshi <vamshis2017@gmail.com>
2022-12-16 23:26:45 +0530

    .

---
+++ src/Component/signup/Signup.js
@@ -0,0 +1,11 @@
+import React, {useState, useEffect} from "react";
+
+
+const SignUp = () => {
+    const [userDetails, setUserDetails] = useState({
+        name: '',
+        email: '',
+        password: '',
+        ConfirmPassword: ''
+    })
+}
